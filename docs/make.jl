25fb33436ca0e4c9ed6c72658151d2d0630fedaf
Nicola Mosco <nicola.mosco@to.infn.it>
2026-02-09 18:01:17 +0100

    docs: gate deployment behind CI check and fix local build warnings

--- docs/make.jl
+++ docs/make.jl
@@ -3,7 +3,10 @@ using GPUBenchmark
 
 makedocs(
     sitename = "GPUBenchmark.jl",
-    format = Documenter.HTML(),
+    format = Documenter.HTML(
+        edit_link = "main",
+        repolink = "https://github.com/nicolamos/GPUBenchmark.jl"
+    ),
     modules = [GPUBenchmark, GPUBenchmark.Core],
     pages = [
         "Home" => "index.md",
@@ -12,6 +15,9 @@ makedocs(
     ]
 )
 
-deploydocs(
-    repo = "github.com/nicolamos/GPUBenchmark.jl.git",
-)
+if get(ENV, "CI", nothing) == "true"
+    deploydocs(
+        repo = "github.com/nicolamos/GPUBenchmark.jl.git",
+        devbranch = "main"
+    )
+end
